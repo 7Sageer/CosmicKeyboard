6b5fa382ca7b80124367515de0d85cba27bd5006
戚涵睿 <12210216@mail.sustech.edu.cn>
2023-12-24 20:37:03 +0800

    Update AutoPlayController and LearningController modules

--- src/LearningController.v
+++ src/LearningController.v
@@ -5,11 +5,15 @@ module LearningConroller(
     input wire next_song,
     input wire prev_song,
     output reg [3:0] song_number,
-    output wire [6:0] display_output,
-    output wire speaker
+    output wire [7:0] display_output,
+    output wire speaker,
+    output wire [6:0] learn_show_led,
+    output wire tub_sel,
+    output wire [7:0] score2,
+    output wire tub_1,
+    output wire tub_2
 );
-
-localparam TOTAL_SONGS = 3;
+localparam TOTAL_SONGS = 2;
 always @(posedge clk or posedge reset) begin
     if (reset) begin
         song_number <= 0;
@@ -21,19 +25,21 @@ always @(posedge clk or posedge reset) begin
         end
     end
 end
-
 LearningPlay learning_play(
     .clk(clk),
     .reset(reset),
     .key_in(key_in),
     .selected_song(song_number),
+    .learn_show_led(learn_show_led),
     .speaker(speaker),
-    .note_out()
+    .tub_1(tub_1),
+    .tub_2(tub_2),
+    .score2(score2)
 );
-
 SevenSegmentDecoder decoder(
     .digit(song_number),
-    .display(display_output)
+    .display(display_output),
+    .tub_sel(tub_sel)
 );
 //todo show the scores
 endmodule
